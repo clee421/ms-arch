1d6af761d26ee7639bd962155a718577808700c1
calvin lee <clee421@gmail.com>
2019-05-27 22:28:27 -0400

    chore: move auth migrations to correct sub folder

--- services/auth/migrations/1558972072_create_auth.up.sql
+++ services/auth/migrations/1558972072_create_auth.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS auth (
   id SERIAL PRIMARY KEY,
-  user_id UUID NOT NULL,
+  user_uuid UUID NOT NULL UNIQUE,
   password text NOT NULL,
   created_at timestamptz DEFAULT now(),
   updated_at timestamptz DEFAULT now()
