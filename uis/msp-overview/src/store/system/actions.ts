f1f7dafde73748574c8dafd594670d3371fe1d4e
Calvin Lee <clee421@gmail.com>
2019-07-23 00:01:34 -0400

    feat: add login action

--- uis/msp-overview/src/store/system/actions.ts
+++ uis/msp-overview/src/store/system/actions.ts
@@ -1,8 +1,34 @@
-import { SystemState, UPDATE_SYSTEM, SystemActionTypes } from './types';
+import { Dispatch } from 'react';
 
-export function updateSystem(newSystemState: SystemState): SystemActionTypes {
+// Request
+import { post } from '../../util/request';
+
+// Types
+import { SystemPayload, UPDATE_SYSTEM, SystemActionTypes } from './types';
+// type DispatchFn = (action: SystemActionTypes) => Dispatch<any>;
+
+export function updateSystemAction(
+  newSystemState: SystemPayload,
+): SystemActionTypes {
   return {
     type: UPDATE_SYSTEM,
     payload: newSystemState,
   };
 }
+
+/* eslint-disable no-console*/
+export const login = (username: string, password: string): any => (
+  dispatch: Dispatch<SystemActionTypes>,
+): any => {
+  return post({ username, password })
+    .then((response: Response): Promise<{ token: string }> => {
+      return response.json();
+    })
+    .then((data: { token: string }): string => {
+      console.log(data);
+      dispatch(updateSystemAction({ token: data.token }));
+      return data.token;
+    })
+    .catch((error: any): any => console.log(error));
+};
+/* eslint-enable no-console*/
